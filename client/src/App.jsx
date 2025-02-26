a0a3b6e9824b2d007350d3f809e93f550d056a55
Ambourland <144298214+Ambourland@users.noreply.github.com>
2025-02-25 21:40:33 -0600

    start

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    {/**add login */}
+    {/**add register */}
+    </>
+  )
+}
+// add route to ToDo page
+
+export default App
