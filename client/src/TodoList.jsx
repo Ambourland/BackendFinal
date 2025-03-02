a3612ac9bd7078d99e2aabd58d068511ec413ad5
Ambourland <144298214+Ambourland@users.noreply.github.com>
2025-03-02 14:32:22 -0600

    update server

--- client/src/TodoList.jsx
+++ client/src/TodoList.jsx
@@ -1,1 +1,144 @@
 // create a todo list
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+function TodoList() {
+  const [data, setData] = useState()
+  const [flag, setFlag] = useState(false)
+  const [edit, setEdit] = useState({
+    todo: ""
+  })
+  const [render, setRender] = useState(false)
+
+  const [newToDo, setNewToDo] = useState(
+    {
+      todo: "",
+      created: Date.now()
+    }
+  )
+  useEffect(() => {
+    console.log("useEFFECT TRIGGERED")
+  }, [data])
+  useEffect(() => {
+    console.warn("USEEFFECT HIT AGAIN")
+    axios({
+      method: "get",
+      url: "http://localhost:3000/gettodos"
+    })
+      .then(res => {
+        console.log("res", res)
+        setData(res.data)
+      })
+      .catch(err => console.log("err", err))
+
+  }, [flag])
+
+  const handleNewToDo = (e) => {
+    console.log("handleNewToDo Hit", e)
+    console.log("handleNewToDo Hit", e.target)
+    console.log("handleNewToDo Hit", e.target.value)
+    setNewToDo((prev) => ({
+      ...prev,
+      todo: e.target.value
+    }))
+  }
+  const handleSubmit = (e) => {
+    console.log("HandleSubmit HIT", newToDo)
+    console.log("i am getting stuff")
+    axios({
+      method: "post",
+      url: "http://localhost:3000/create",
+      data: newToDo
+
+    })
+      .then(res => {
+        console.log("res", res)
+        setFlag(!flag)
+      })
+      .catch(err => console.log(err))
+
+  }
+  const handleDelete = (e) => {
+
+    console.log("DEL Hit e.target.e", e.target.id)
+
+    axios({
+      method: "delete",
+      url: `http://localhost:3000/delete/${e.target.id}`
+    })
+      .then(res => {
+        console.log("re", res)
+        console.log("RES", res.data._id)
+        setData((prev) => prev.filter((item) => item._id != res.data._id))
+      })
+      .catch(err => console.log(err))
+  }
+
+  const handleEdit = () => {
+    setRender(!render)
+  }
+
+  const handleEditSubmit = (e) => {
+    console.log("HandleEdit HIT", e.target.id)
+    axios({
+      method: "put",
+      url: `http://localhost:3000/edit/${e.target.id}`,
+      data: edit
+    })
+      .then(res => {
+        console.log("$$$$$$$$", res)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const handleEditChange = (e) => {
+    console.log("handleEditChange  HIT", e.target.value)
+    setEdit({ todo: e.target.value })
+  }
+
+  return (
+    <>
+      {console.warn("render", render)}
+   
+      <input onChange={(e) => handleNewToDo(e)} />
+      <button onClick={(e) => handleSubmit(e)}>Submit</button>
+
+      {data && data.sort((a, b) => b.created - a.created).map((item) => {
+        return (
+
+          <div key={item._id} style={{ marginBottom: "20px" }}>
+
+            <div style={{ border: '2px solid red' }}>
+              {render
+                ?
+                (
+                  <div>
+                    <input
+                      defaultValue={item.todo || ""}
+                      onChange={(e) => handleEditChange(e)}
+                    >
+                    </input>
+
+                    <button
+                      id={item._id}
+                      onClick={(e) => handleEditSubmit(e)}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                )
+                :
+                (
+                  <p> {item.todo}</p>
+                )
+              }
+              <button id={item._id} onClick={(e) => handleDelete(e)}>delete</button>
+              <button id={item._id} onClick={(e) => handleEdit(e)}>edit</button>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+export default TodoList
